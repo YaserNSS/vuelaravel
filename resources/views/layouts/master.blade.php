7120c9b71755451c7013706e6940ffcb97fcbec8
MacBook <macbook@MacBooks-MacBook-Pro.local>
2020-03-17 13:58:47 +0300

    Lec 5 Configure Vue Router

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,12 +11,14 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <meta http-equiv="x-ua-compatible" content="ie=edge">
 
   <title>AdminLTE 3 | Starter</title>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
   <link rel="stylesheet" href="/css/app.css">
 
 </head>
 <body class="hold-transition sidebar-mini">
-<div class="wrapper">
+<div class="wrapper" id="app">
 
   <!-- Navbar -->
   <nav class="main-header navbar navbar-expand navbar-white navbar-light">
@@ -69,12 +71,12 @@ scratch. This page gets rid of all links and provides the needed markup only.
                with font-awesome or any other icon font library -->
 
         <li class="nav-item">
-            <a href="#" class="nav-link">
+            <router-link to="/dashboard" class="nav-link">
                 <i class="nav-icon fas fa-tachometer-alt"></i>
                 <p>
                     Dashboard
                 </p>
-            </a>
+            </router-link>
         </li>
 
           <li class="nav-item has-treeview menu-open">
@@ -102,12 +104,12 @@ scratch. This page gets rid of all links and provides the needed markup only.
           </li>
 
         <li class="nav-item">
-            <a href="#" class="nav-link">
+            <router-link to="/profile" class="nav-link">
                 <i class="nav-icon fas fa-user"></i>
                 <p>
                     Profile
                 </p>
-            </a>
+            </router-link>
         </li>
         <li class="nav-item">
             <a href="#" class="nav-link">
@@ -132,6 +134,9 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Main content -->
     <div class="content">
       <div class="container-fluid">
+
+            <router-view></router-view>
+
       </div><!-- /.container-fluid -->
     </div>
     <!-- /.content -->
