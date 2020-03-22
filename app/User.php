bf3a33d14bf174790c2ec960f8cdae070bc29424
MacBook <macbook@MacBooks-MacBook-Pro.local>
2020-03-23 01:42:33 +0300

    Lec 12 Insert User Data using Axios

--- app/User.php
+++ app/User.php
@@ -10,14 +10,17 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'bio', 'photo', 'type'
     ];
+
 
     /**
      * The attributes that should be hidden for arrays.
